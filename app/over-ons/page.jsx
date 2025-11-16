9d4b1d128ed5924f838a034d9ff2f93bdde28302
Bunyaminbolukbas <bunyaminbolukbas@outlook.com>
2025-11-16 15:32:08 +0100

    over ons semi gefixt

--- app/over-ons/page.jsx
+++ app/over-ons/page.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Header64 } from "@/components/sections/header-64";
 import { Layout22 } from "@/components/sections/layout-22";
 import { Team12 } from "@/components/sections/team-12";
-import { Faq14 } from "@/components/sections/faq-14";
-import { Testimonial17 } from "@/components/sections/testimonial-17";
+import { KernwoordenHome } from "@/components/sections/kernwoorden-home";
+import { TestimonialWithStory } from "@/components/sections/testimonial-with-story";
 
 export default function OverOnsPage() {
   return (
@@ -11,8 +11,8 @@ export default function OverOnsPage() {
       <Header64 />
       <Layout22 />
       <Team12 />
-      <Faq14 />
-      <Testimonial17 />
+      <KernwoordenHome />
+      <TestimonialWithStory />
     </div>
   );
 }
